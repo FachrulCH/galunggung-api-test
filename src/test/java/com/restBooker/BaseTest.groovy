d3a91ee983b20b6ee21585a5776c63efea94b903
SDET macmini <sdet.sleekr@gmail.com>
2018-03-09 18:51:19 +0700

    integration to reporting

--- src/test/java/com/restBooker/BaseTest.groovy
+++ src/test/java/com/restBooker/BaseTest.groovy
@@ -1,6 +1,8 @@
 package com.restBooker
 
 import com.restBooker.steps.BookingSteps
+import groovy.time.TimeCategory
+import groovy.time.TimeDuration
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -8,8 +10,17 @@ abstract class BaseTest extends Specification{
 
     @Shared
     protected BookingSteps bookingSteps = new BookingSteps()
+    protected Date startTime;
 
     def cleanup(){
-        println("Cleanup after test")
+
+        def endTime = new Date()
+        TimeDuration duration = TimeCategory.minus(endTime, startTime)
+        println "Done testing for => ${duration} \n"
+    }
+
+    def setup(){
+        startTime = new Date()
+        println "Ready for testing! ${specificationContext.getCurrentIteration().name}"
     }
 }
--- src/test/java/com/restBooker/BookingTest.groovy
+++ src/test/java/com/restBooker/BookingTest.groovy
@@ -6,21 +6,22 @@ class BookingTest extends BaseTest{
     protected String idNewBooking
 
     def "should return default booking"(){
-        expect:
+        expect: 'return all bookings'
             bookingSteps.userCanGetAllBookings()
     }
 
     def "should return detail booking of id 1"(){
-        when:
+        when: 'open detail booking'
             def resp = bookingSteps.userCanViewDetailBooking(1)
 
-        then:
+        then: 'show detail booking of Jim'
             resp.getStatusCode() == 200
-            println "time: "+resp.getTime()
+            resp.path("firstname") == 'Jim'
+            println "\t Response time: "+resp.getTime()
     }
 
     def "should create new booking"(){
-        given:
+        given: 'Prepared data for insert'
             def buking = new Booking(firstname: 'Asep',
                     lastname: 'Sukoco',
                     totalprice: 16000,
@@ -28,16 +29,12 @@ class BookingTest extends BaseTest{
                     additionalnotes: 'I miss you'
             )
 
-        when:
+        when: 'submit the booking'
             def resp = bookingSteps.userCanCreateNewBooking(buking)
             idNewBooking = resp.body().path("bookingid") // assignment can not inside block of then:
-            println "time: "+resp.getTime()
+            println "\t Response time: "+resp.getTime()
 
-        and: 'View saved detail'
-            def detailBooking = bookingSteps.userCanViewDetailBooking(idNewBooking)
-            println "time: "+detailBooking.getTime()
-
-        then:
+        then: 'booking created'
             resp.getStatusCode() == 200
             // show all response:  println resp.getBody().prettyPrint()
             resp.body().path("booking.firstname") == buking.firstname
@@ -45,14 +42,22 @@ class BookingTest extends BaseTest{
 
             //println "Booking id sekarang => "+idNewBooking
 
+        when: 'open detail of new created booking'
+            def detailBooking = bookingSteps.userCanViewDetailBooking(idNewBooking)
+            println "\t Response time: "+detailBooking.getTime()
+
+        then: 'saved data should correct'
             //println detailBooking resp.getBody().print()
+            detailBooking.path("firstname") == buking.firstname
             detailBooking.path("totalprice") == buking.totalprice
+            //detailBooking.path("bookingdates.checkin") == buking.bookingdates.checkin
+
     }
 
     def "should be able to delete booking"(){
-        when:
+        when: 'delete booking id 5'
             def resp = bookingSteps.userCanDeleteBooking(id: 5)
-        then:
+        then: 'success delete'
             resp.getStatusCode() == 201
             println resp.body().prettyPrint()
     }
@@ -60,7 +65,7 @@ class BookingTest extends BaseTest{
     def "should not be able to re-delete booking"(){
         when: 'delete using same id as previous'
             def resp = bookingSteps.userCanDeleteBooking(id: 5)
-        then:
+        then: 'fail to delete'
             resp.getStatusCode() == 405
             println resp.body().prettyPrint()
     }
